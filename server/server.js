fdfddea039a65e71d7bfb0de4f9a27d17ce12128
sayak roy <08sayakroy@gmail.com>
2025-12-08 12:01:29 +0530

    added email functionality

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
-import {inngest, functions} from './inngest/index.js';
-import {serve} from 'inngest/express'
+import { inngest, functions } from './inngest/index.js';
+import { serve } from 'inngest/express'
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,11 +13,13 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get('/', (req, res)=> {
     res.send ('server is running');
 })
 app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/user', userRouter)
 
 app.listen(PORT, ()=> {
     console.log(`server is running on port ${PORT}`)
